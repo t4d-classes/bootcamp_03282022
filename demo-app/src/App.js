71b1e56a0abed201fd428fae3a5c088da07d9229
Eric Greene <eric@t4d.io>
2022-03-28 17:27:17 -0400

    add initial layout

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Layout } from './components/Layout';
 import { HelloWorld } from './components/HelloWorld';
 
 
 export function App() {
 
     return (
-        <HelloWorld />
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="color-tool" element={<ColorTool />} />
+            </Route>
+        </Routes>
     );
 
 }
