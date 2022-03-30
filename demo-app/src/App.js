1f41d5be655c639028c4f849fec7789a78c16c1d
Eric Greene <eric@t4d.io>
2022-03-30 11:46:22 -0400

    add day 2 solutions

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -5,6 +5,12 @@ import { Home } from './components/Home';
 import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
 
+const colorList = [
+    { id: 1, name: 'red', hexcode: 'ff0000' },
+    { id: 2, name: 'green', hexcode: '00ff00' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+];
+
 const carList = [
     { id: 1, make: 'Bugati', model: 'Chiron Supersport', year: 2022, color: 'blue', price: 3825000 },
     { id: 2, make: 'Ford', model: 'Fusion Hybrid', year: 2020, color: 'red', price: 45000 },
@@ -17,7 +23,7 @@ export function App() {
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path="color-tool" element={<ColorTool />} />
+                <Route path="color-tool" element={<ColorTool colors={colorList} />} />
                 <Route path="car-tool" element={<CarTool cars={carList} />} />
             </Route>
         </Routes>
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -1,37 +1,12 @@
-export function ColorTool() {
+import { ToolHeader } from './ToolHeader';
+import { ColorList } from './ColorList';
 
-    // primitives
-    // - string*
-    // - number*
-    // - boolean*
-    // - symbol
-    // - bigint
-    // - undefined*
-    // - null*
-
-    // object
-    // everything else is an object - Object Literal, Arrays, Functions, Date, Maps, Sets, Promises, etc...
-
-    const colors = [
-        { id: 1, name: 'red', hexcode: 'ff0000' },
-        { id: 2, name: 'green', hexcode: '00ff00' },
-        { id: 3, name: 'blue', hexcode: '0000ff' },
-    ];
+export function ColorTool(props) {
 
     return (
         <>
-            <header>
-                <h3>Color Tool</h3>
-            </header>
-            <ul>
-                {/*
-                    - purpose of the key is connect model data (color) to the element (li)
-                    - why? to optimize re-rendering ** explain later **
-                    - the key should be number or string that is unique within the set of model data
-                    - the key is applied to dynamically generated siblings
-                */}
-                {colors.map(color => <li key={color.id}>{color.name}</li>)}
-            </ul>
+            <ToolHeader headerText='Color Tool' />
+            <ColorList colors={props.colors} />
         </>
     );
 
