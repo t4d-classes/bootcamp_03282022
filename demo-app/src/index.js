d54a24494e028918d2d09ef42f41f0637e3b3410
Eric Greene <eric@t4d.io>
2022-04-06 14:49:28 -0400

    add all done callback exercise

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,19 +1,19 @@
-import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { render } from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
-import { App } from './App';
+// import { App } from './App';
 
-import { ColorToolStoreProvider } from './contexts/colorToolStoreContext';
+// import { ColorToolStoreProvider } from './contexts/colorToolStoreContext';
 
-render(
-    // JSX
-    // React.createElement(HelloWorld)
-    <Router>
-        <ColorToolStoreProvider>
-            <App />
-        </ColorToolStoreProvider>
-    </Router>,
-    // #root is a CSS Selector, the # symbol means to select by id
-    document.querySelector('#root'),
-);
+// render(
+//     // JSX
+//     // React.createElement(HelloWorld)
+//     <Router>
+//         <ColorToolStoreProvider>
+//             <App />
+//         </ColorToolStoreProvider>
+//     </Router>,
+//     // #root is a CSS Selector, the # symbol means to select by id
+//     document.querySelector('#root'),
+// );
 
